2bc37998205e75a8b55bcc8ea7c64785f2f27ec9
romain <romain@LAPTOP-CHT7AOJR>
2019-09-09 16:13:32 +0200

    agenda rectif

--- agenda/src/main/java/com/example/models/Agenda.java
+++ agenda/src/main/java/com/example/models/Agenda.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Agenda {
@@ -16,6 +17,9 @@ public class Agenda {
 	
 	
 	public Calendar dateDebut;
+	
+	@ManyToOne(mappedBy="agenda")
+	public Rdv rdv;
 	
 	public Long getId() {
 		return id;
