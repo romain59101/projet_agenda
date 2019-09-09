0211468d1ffa3140ce2b6c7546e01ebf2078b79e
IN-LL-010 <IN-LL-010@IN-LL-010>
2019-09-09 16:13:25 +0200

    2

--- agenda/src/main/java/com/example/models/Client.java
+++ agenda/src/main/java/com/example/models/Client.java
@@ -1,5 +1,8 @@
 package com.example.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Client {
